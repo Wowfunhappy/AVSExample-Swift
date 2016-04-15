e61ba432ee68f3628e278e262ce09ede6fcb7fd6
Jonathan <Wowfunhappy@gmail.com>
2016-04-15 18:25:07 -0400

    Update update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -2,19 +2,13 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-rm -rf public
-mkdir public
-
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-# build (CHANGE THIS)
-make
-
 # deploy
-cd public
+cd build
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@$github.com/Wowfunhappy/AVSExample-Swift.git" master:build > /dev/null 2>&1
